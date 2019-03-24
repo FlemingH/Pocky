6c0c6603f2e3a2997875deb5e9b01538f08c9782
Fleming <16278383@qq.com>
2019-03-24 13:17:22 +0800

    frame up spring boot project

---
+++ src/main/java/indi/fleming/pocky/PockyApplication.java
@@ -0,0 +1,13 @@
+package indi.fleming.pocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PockyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PockyApplication.class, args);
+    }
+
+}
